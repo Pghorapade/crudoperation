582fd60968e7fb9b1f8ce5fc18ba2d626e58c675
Digambar Shinde <ghorapadep08@gmail.com>
2019-05-09 21:35:52 +0530

    update file

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -12,6 +12,7 @@ module.exports = function(passport) {
     // passport needs ability to serialize and unserialize users out of session
 
     // used to serialize the user for the session
+    
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
